8af15a3c5e801c576ea7916f805566b9c93e280f
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-03-25 01:24:14 +0700

    Update TagView.swift

--- GuidEducation/Views/Components/TagView.swift
+++ GuidEducation/Views/Components/TagView.swift
@@ -15,25 +15,27 @@ struct TagView: View {
             Image("\(tagName)-icon")
                 .resizable()
                 .scaledToFit()
-                .frame(maxWidth: 10)
+                .frame(maxWidth: 10, maxHeight: 10)
+//                .background(Color.pink)
             Text(tagName)
                 .font(.system(size: 12))
                 .foregroundColor(.black)
+//                .background(Color.purple)
         }
-        
         .padding(4)
         .padding(.horizontal, 5)
+//        .clipShape(.capsule, style: .init(eoFill: true, antialiased: true))
         .background {
             RoundedRectangle(cornerRadius: 50)
-                .stroke(Color.gray, lineWidth: 2)
+                .stroke(Color.gray, lineWidth: 1)
                 .background(Color.white)
         }
-        .cornerRadius(50)
+//        .cornerRadius(10)
     }
 }
 
-//struct TagView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        TagView(tagName: "Mathematics")
-//    }
-//}
+struct TagView_Previews: PreviewProvider {
+    static var previews: some View {
+        TagView(tagName: "Math-icon")
+    }
+}
