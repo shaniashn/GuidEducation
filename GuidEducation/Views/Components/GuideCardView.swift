18655cf9b8e962af9786fd37c25111ac97f845a5
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-03-25 01:24:12 +0700

    Update GuideCardView.swift

--- GuidEducation/Views/Components/GuideCardView.swift
+++ GuidEducation/Views/Components/GuideCardView.swift
@@ -7,108 +7,50 @@
 
 import SwiftUI
 
-struct Guideliness: View {
-    
-    var body: some View {
-        NavigationView {
-            ZStack {
-                RoundedRectangle(cornerRadius: 12)
-                .fill(Color("guidelines-yellow"))
-                .frame(minWidth: 358, maxWidth: .infinity, minHeight: 82, maxHeight: 82)
-                
-                Image("guide-img1")
-                    .resizable()
-//                    .scaledToFit()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(RoundedRectangle(cornerRadius: 12))
-                    .frame(height: 100)
-                
-//                Text("Create activity that engage learners")
-//                    .font(.system(size: 15))
-//                    .fontWeight(.bold)
-//                    .frame(maxWidth: 200, maxHeight: 35)
-//                HStack {
-//                    Text("Create activity that engage learners")
-//                    .font(.system(size: 15))
-//                    .fontWeight(.bold)
-//                    .frame(maxWidth: 200, maxHeight: 35)
-//                    .lineLimit(2)
-//                    Spacer()
-//                    Image(systemName: "chevron.right")
-//                }
-//                .frame(minWidth: 358, maxWidth: .infinity, minHeight: 82, maxHeight: 82)
-//                .padding()
-            }
-            
-        }
-        .padding()
-    }
-}
-
-struct Guideliines: View {
-    var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 12)
-            .fill(Color("guidelines-yellow"))
-            .frame(minWidth: 358, maxWidth: .infinity, minHeight: 82, maxHeight: 82)
-//            Image("guide-img1")
-//                .resizable()
-//                .scaledToFit()
-//                .clipShape(RoundedRectangle(cornerRadius: 12))
-//                .frame(height: 300)
-//                .clipped()
-//                .offset(x: -20)
-            
-            
-            
-            HStack {
-                Image("guide-img1")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 200)
-                    .clipShape(RoundedRectangle(cornerRadius: 12))
-                    .frame(height: 80)
-                    .clipped()
-//                    .offset(x: -20)
-                Spacer()
-                Spacer()
-            }
-//            .frame(width: 600)
-            .clipShape(RoundedRectangle(cornerRadius: 12))
-            .frame(width: 550)
-//            .clipped()
-                
-        }
-    }
-}
-
 struct GuideCardView: View {
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 12)
             .fill(Color("guidelines-yellow"))
             .frame(minWidth: 358, maxWidth: .infinity, minHeight: 82, maxHeight: 82)
+            .clipShape(.rect(cornerRadius: 12, style: .continuous))
+            .overlay {
+                HStack(){
+                    Image("guide-img1")
+                        .mask {
+                            maskRect
+                        }
+                        
+                    Spacer()
+                }
+            }
             HStack {
-                Image("guide-img1")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 82)
-                    .clipShape(RoundedRectangle(cornerRadius: 12))
-                    .frame(width: 200)
-//                    .frame(height: 100)
-                    .clipped()
-                    .offset(x: -70)
                 Spacer()
                 Text("Create activity that engage learners")
-                    .font(.system(size: 15))
-                                        .fontWeight(.bold)
-                                        .frame(maxWidth: 200, maxHeight: 35)
+                    .font(.system(size: 17, weight: .bold))
+                    .frame(width: 250)
+                NavigationLink {
                     
-              
+                } label: {
+                    Image(systemName: "chevron.right")
+                        .foregroundStyle(Color.black)
+                }
+
             }
+            .padding(.trailing)
             
         }
  
+    }
+    
+}
+
+extension GuideCardView {
+    private var maskRect: some View {
+        RoundedRectangle(cornerRadius: 12)
+            .fill(Color("guidelines-yellow"))
+            //untuk mask bagian frame width dihilangkan karena malah mengganggu bagian masknya
+            .frame(minHeight: 82, maxHeight: 82)
     }
 }
 
