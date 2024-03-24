5c2d4d4e61a1543d84227f7c486c41b425329528
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-03-25 01:24:05 +0700

    Update ActivityViewModel.swift

--- GuidEducation/ViewModels/ActivityViewModel.swift
+++ GuidEducation/ViewModels/ActivityViewModel.swift
@@ -8,19 +8,16 @@
 import Foundation
 
 class ActivityViewModel: ObservableObject {
-    @Published var activities: [Activity]
-    @Published var tags: [Tag]
+    @Published var activities: [Activity] = ActivityData.activityData
+    @Published var tags: [Tag] = ActivityData.theTags
     
     init() {
         //kalo ini bisa diganti dari sumbernya yaitu ActivityData, berarti melanggar aturan MVVM krn harusnya yg boleh mengubah let activity itu cuma viewmodel
-        let activity = ActivityData.activityData
-        let tags = ActivityData.theTags
-        self.activities = activity
+//        let activity = ActivityData.activityData
+//        let tags = ActivityData.theTags
+        self.activities = activities
         self.tags = tags
     }
     
-    func a() {
-        
-    }
     
 }
