9f38708f1a467b6fc555ed52fa2687ca009483c0
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-03-27 14:40:38 +0700

    Create ReflectionView.swift

---
+++ GuidEducation/Views/ReflectionView.swift
@@ -0,0 +1,30 @@
+//
+//  ReflectionView.swift
+//  GuidEducation
+//
+//  Created by Sha Nia Siahaan on 27/03/24.
+//
+
+import SwiftUI
+
+struct ReflectionView: View {
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                VStack {
+                    Image("orange")
+                    Spacer()
+                }
+                
+                VStack {
+                    
+                }
+            }
+            .ignoresSafeArea()
+        }
+    }
+}
+
+#Preview {
+    ReflectionView()
+}
