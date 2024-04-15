b6020f0039ead27738b28eb01f2b081a078baaf5
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-04-15 22:52:37 +0700

    Update GuidEducationApp.swift

--- GuidEducation/App/GuidEducationApp.swift
+++ GuidEducation/App/GuidEducationApp.swift
@@ -10,10 +10,12 @@ import SwiftUI
 @main
 struct GuidEducationApp: App {
     @StateObject var vm = ActivityViewModel()
+//    @StateObject var gvm = GuidelinesViewModel()
     var body: some Scene {
         WindowGroup {
             HomeView()
                 .environmentObject(vm)
+//                .environmentObject(gvm)
         }
     }
 }
