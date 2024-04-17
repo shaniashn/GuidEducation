08769d2c9efbd2b5b08198f009d3a5f8155af3c2
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-04-17 23:13:15 +0700

    Update ActivityViewModel.swift

--- GuidEducation/ViewModels/ActivityViewModel.swift
+++ GuidEducation/ViewModels/ActivityViewModel.swift
@@ -10,7 +10,9 @@ import Foundation
 class ActivityViewModel: ObservableObject {
     @Published var cards: [Card] = ActivityData.activityData
     @Published var tags: [Tag] = ActivityData.theTags
+//    @Published var activity: [Activity] = 
     var guides: [Guidelines] = ActivityData.guidelinesData
+//    @Published var answers: [Answers] = answrs1
     
     init() {
         //kalo ini bisa diganti dari sumbernya yaitu ActivityData, berarti melanggar aturan MVVM krn harusnya yg boleh mengubah let activity itu cuma viewmodel
@@ -20,10 +22,22 @@ class ActivityViewModel: ObservableObject {
         self.tags = tags
     }
     
-    func findIdGuidelines(guide: Guidelines) -> String {
-        
-        
-        return guide.type
-    }
     
+    
+//    func findIdGuidelines(guide: Guidelines) -> String {
+//        return guide.type
+//    }
+    
+//    func changeSelected(ans: Answers) -> Answers {
+//        var result: Bool
+//        if var index = answers.firstIndex(where: { $0.id == ans.id }) {
+//            result = answers[index].isSelected
+//        } else {
+//            print("index not found")
+//        }
+//        
+//        return answers[index].isSelected
+        
+//        return Answers(id: ans.id, ans: ans.ans, isSelected: !ans.isSelected)
+//    }
 }
