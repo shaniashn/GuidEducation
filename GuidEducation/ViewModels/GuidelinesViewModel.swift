07830f2d34bb02aa37bb07a0c1c0873c661f514b
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-04-18 23:59:53 +0700

    Update GuidelinesViewModel.swift

--- GuidEducation/ViewModels/GuidelinesViewModel.swift
+++ GuidEducation/ViewModels/GuidelinesViewModel.swift
@@ -7,6 +7,6 @@
 
 import Foundation
 
-class GuidelinesViewModel: ObservableObject {
-    @Published var guides: [Guidelines] = []
-}
+//class GuidelinesViewModel: ObservableObject {
+//    @Published var guides: [Guidelines] = []
+//}
