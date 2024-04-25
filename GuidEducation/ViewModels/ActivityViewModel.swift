67c00eb9bc365a0b8148ce49c4105a9aa9cd8b43
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-04-25 23:26:49 +0700

    Update ActivityViewModel.swift

--- GuidEducation/ViewModels/ActivityViewModel.swift
+++ GuidEducation/ViewModels/ActivityViewModel.swift
@@ -12,6 +12,7 @@ class ActivityViewModel: ObservableObject {
     @Published var tags: [Tag] = ActivityData.theTags
     @Published var activity: Activity = ActivityData.activityData
     
+//    @Published var currentWeek = []
     
     init() {
         //kalo ini bisa diganti dari sumbernya yaitu ActivityData, berarti melanggar aturan MVVM krn harusnya yg boleh mengubah let activity itu cuma viewmodel
@@ -22,5 +23,33 @@ class ActivityViewModel: ObservableObject {
         self.activity = activity
     }
     
+    func getWeekOfDay() -> [DateComponents] {
 
+        var dateAWeek: [DateComponents] = []
+        let calendar = Calendar(identifier: .gregorian)
+        let dateComponent = calendar.dateComponents([
+            .calendar,
+            .month,
+            .weekday,
+            .weekOfMonth,
+            .day
+        ], from: Date())
+        
+        let weekOfMonthPoint = dateComponent.weekOfMonth ?? 0
+        let monthPoint = dateComponent.month ?? 0
+        
+        for i in 1..<8 {
+            let datePerWeek = DateComponents(year: 2024, month: monthPoint, weekday: i, weekOfMonth: weekOfMonthPoint)
+            
+            let calendarPerWeek = calendar.date(from: datePerWeek)
+            
+            let dateNumber = Calendar.current.dateComponents([.day], from: calendarPerWeek ?? Date())
+            dateAWeek.append(dateNumber)
+//            print(calendarPerWeek)
+//            dateAWeek.append(dateNumber.day ?? 0)
+            
+        }
+        return dateAWeek
+    }
+    
 }
