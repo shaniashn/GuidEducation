33bee66e6e9d9203767f3dd383a631c59a55c949
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-05-01 21:15:03 +0700

    add getNextWeek()

--- GuidEducation/ViewModels/ActivityViewModel.swift
+++ GuidEducation/ViewModels/ActivityViewModel.swift
@@ -12,8 +12,10 @@ class ActivityViewModel: ObservableObject {
     @Published var tags: [Tag] = ActivityData.theTags
     @Published var activity: Activity = ActivityData.activityData
     
-//    @Published var dateAWeek: [Date] = []
+    @Published var dateAWeek: [Date] = []
     @Published var currentDate: Date = Date()
+    
+    @Published var weekOfMonthPointt = 1
     
     init() {
         //kalo ini bisa diganti dari sumbernya yaitu ActivityData, berarti melanggar aturan MVVM krn harusnya yg boleh mengubah let activity itu cuma viewmodel
@@ -22,10 +24,44 @@ class ActivityViewModel: ObservableObject {
         self.cards = cards
         self.tags = tags
         self.activity = activity
+        getWeekOfDay()
     }
     
-    func getWeekOfDay() -> [Date] {
-        var dateAWeek: [Date] = []
+//      MARK: - Versi 1
+//    func getWeekOfDay() -> [Date] {
+//        var dateAWeek: [Date] = []
+//        let calendar = Calendar(identifier: .gregorian)
+//        let dateComponent = calendar.dateComponents([
+//            .calendar,
+//            .month,
+//            .weekday,
+//            .weekOfMonth,
+//            .day
+//        ], from: currentDate)
+//        
+//        let weekOfMonthPoint = dateComponent.weekOfMonth ?? 0
+//        let monthPoint = dateComponent.month ?? 0
+//        
+//        for i in 1..<8 {
+//            let datePerWeek = DateComponents(year: 2024, month: monthPoint, weekday: i, weekOfMonth: weekOfMonthPoint)
+//            
+//            if let calendarPerWeek = calendar.date(from: datePerWeek) {
+//                dateAWeek.append(calendarPerWeek)
+////                dateAWeek.append(Date())
+//            }
+//            
+////            let dateNumber = Calendar.current.dateComponents([.day], from: calendarPerWeek ?? Date())
+//            
+////            dateAWeek.append(dateNumber)
+////            dateAWeek.append(dateNumber.day ?? 0)
+//            
+//        }
+//        return dateAWeek
+//    }
+    
+//      MARK: - Versi 2
+    func getWeekOfDay() {
+//        var dateAWeek: [Date] = []
         let calendar = Calendar(identifier: .gregorian)
         let dateComponent = calendar.dateComponents([
             .calendar,
@@ -33,26 +69,52 @@ class ActivityViewModel: ObservableObject {
             .weekday,
             .weekOfMonth,
             .day
-        ], from: Date())
+        ], from: currentDate)
         
         let weekOfMonthPoint = dateComponent.weekOfMonth ?? 0
         let monthPoint = dateComponent.month ?? 0
         
+        self.weekOfMonthPointt = weekOfMonthPoint
+        
         for i in 1..<8 {
-            let datePerWeek = DateComponents(year: 2024, month: monthPoint, weekday: i, weekOfMonth: weekOfMonthPoint)
+            let datePerWeek = DateComponents(year: 2024, month: monthPoint, weekday: i, weekOfMonth: weekOfMonthPointt)
             
             if let calendarPerWeek = calendar.date(from: datePerWeek) {
-//                dateAWeek.append(calendarPerWeek)
-                dateAWeek.append(Date())
+                dateAWeek.append(calendarPerWeek)
+//                dateAWeek.append(Date())
             }
             
 //            let dateNumber = Calendar.current.dateComponents([.day], from: calendarPerWeek ?? Date())
             
 //            dateAWeek.append(dateNumber)
 //            dateAWeek.append(dateNumber.day ?? 0)
-            
         }
-        return dateAWeek
+//        return dateAWeek
+        print("dateAWeek -> ", dateAWeek)
+    }
+    
+    func getNextWeek(){
+        dateAWeek.removeAll()
+        print("dateAWeek --> ", dateAWeek)
+        let calendar = Calendar(identifier: .gregorian)
+        if let dateNext = calendar.date(byAdding: .weekOfMonth, value: 1, to: currentDate) {
+//            print(currentDate)
+//            print(dateNext)
+            currentDate = dateNext
+        }
+        
+        getWeekOfDay()
+    }
+    
+    func getPrevWeek(){
+        dateAWeek.removeAll()
+        print("dateAWeek --> ", dateAWeek)
+        let calendar = Calendar(identifier: .gregorian)
+        if let dateBefore = calendar.date(byAdding: .weekOfMonth, value: -1, to: currentDate) {
+            currentDate = dateBefore
+        }
+        
+        getWeekOfDay()
     }
     
     func formatDate(date: Date, format: String) -> String {
@@ -62,8 +124,9 @@ class ActivityViewModel: ObservableObject {
         return formatter.string(from: date)
     }
     
-    func isTodayDate(date: Date) {
-        let checkIsTodayDate = Calendar.current.isDateInToday(date)
+    func makeTodayDate(date: Date) -> Bool {
+//        let checkIsTodayDate = Calendar.current.isDateInToday(date)
         let checkDate = Calendar.current.isDate(currentDate, inSameDayAs: date)
+        return checkDate
     }
 }
